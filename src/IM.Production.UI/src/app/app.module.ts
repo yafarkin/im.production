16d971555009420793995e61463fcaf16f00f029
Aleksei Khripunov <aleksei.khripunov@outlook.com>
2020-01-27 23:59:04 +0400

    #15 Создан компонет входа в игру

--- src/IM.Production.UI/src/app/app.module.ts
+++ src/IM.Production.UI/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { OldAppComponent } from './old-app/app.component';
 import { AppComponent } from './app/app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    OldAppComponent,
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        OldAppComponent,
+        AppComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
